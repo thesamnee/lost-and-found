8181f7143d574069844b00d4bd3e881b78624c2b
Sam Nee <samuel.r.nee@gmail.com>
2020-04-15 19:28:13 -0400

    added CRUD components

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { PostItemComponent } from './post-item/post-item.component';
+import { EditItemComponent } from './edit-item/edit-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
+    ItemDetailComponent,
+    PostItemComponent,
+    EditItemComponent
   ],
   imports: [
     BrowserModule,
